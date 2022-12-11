f3756c0f3dea7652a5d48c4711a72b222569780b
Daniiyk <dani.kuroda@gmail.com>
2022-12-11 18:10:08 -0300

    Última atualizaçao do desafio Pokedex

--- assets/js/mainDetalhe.js
+++ assets/js/mainDetalhe.js
@@ -2,24 +2,39 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
-const limit = 12;
+const limit = 3;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${("000" + pokemon.number).slice(-3)}</span>
+                <div class="detailPoke">
+            <a href="detalhe.html">
+            <img src="${pokemon.photo}"
+                 alt="${pokemon.name}"></a>
+                 </div>
+                     <div class="detailName">
             <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
+                     </div>
+                <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<span class="type ${type}">${type}</span>`).join('')}
                 </ol>
-                <a href="detalhe.html">
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}"></a>
-            </div>
+                </div>
+                    <div class="detailBox">
+                    <h4>Ability</h4>
+                            <p>Species <b>${pokemon.id}</b></p>
+                            <p>Height <b>${pokemon.type}</b></p>
+                            <p>Weight <b>${pokemon.type[1]}</b></p>
+                            <p>Abilities <b>${pokemon.name}</b></p>
+                    <h4>About</h4>
+                            <p>Egg Groups <b>${pokemon.types}</b></p>
+                            <p>Egg Cycle <b>${pokemon.name}</b></p>
+                     </div>
+
         </li>
+
    
     `
 }
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -9,6 +9,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
+
     pokemon.types = types
     pokemon.type = type
 
@@ -23,7 +24,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 12) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
